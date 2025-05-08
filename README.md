8fc07b279d59c9269e31f540f24e24c617973251
Akhil Singh <akhil_singh09@yahoo.com>
2025-05-08 13:46:20 +0530

    File Upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# Dialogue
-Chat Application
---
+++ app/src/main/java/com/cometchat/kotlinsampleapp/AppConstants.kt
@@ -0,0 +1,11 @@
+package com.cometchat.kotlinsampleapp
+
+object AppConstants {
+
+    const val APP_ID = "2665873729a65dfc"
+
+    const val AUTH_KEY = "1b2257ac5039197d8893038cc0fe3a6e905bb2f2"
+
+    const val REGION = "in"
+
+}
